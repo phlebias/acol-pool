53968f016f3b174fa7c1364a5a03294fa6f9702f
phlebias <william.burrows1@gmail.com>
2025-03-03 23:32:49 +0000

    Move Firebase config to environment variables for better security

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTEuGAD2MDaShqbXR1pBQ-SY2XW_7Q6XA",
-  authDomain: "acol-pool.firebaseapp.com",
-  projectId: "acol-pool",
-  storageBucket: "acol-pool.firebasestorage.app",
-  messagingSenderId: "659103503233",
-  appId: "1:659103503233:web:93c8415021dbaf3bfc4e80",
-  measurementId: "G-ZQHM11VWWH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
